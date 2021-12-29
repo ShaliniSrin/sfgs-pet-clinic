cf7f74c93dbb6b21656a663a28b4ccf10297d9e6
Shalini.Kolipaka <shalini.kolipaka@cashe.co.in>
2021-12-30 03:35:22 +0530

    project set up

---
+++ src/main/java/guru/springframework/sfgspetclinic/SfgsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgsPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgsPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfgspetclinic/SfgsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgsPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
